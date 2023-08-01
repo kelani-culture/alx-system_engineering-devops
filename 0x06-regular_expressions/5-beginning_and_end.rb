952ef1e38d49d1da97a16f5d317193e85ef807ac
Null <kelanidarasimi9@gmail.com>
2023-08-01 20:57:16 +0100

    Not quite HBTN yet

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+def reg_pattern(arg)
+  patt = /^h.+n$/
+  comb_arg = arg.join(" ")
+
+  matches = comb_arg.scan(patt)
+  if matches.empty?
+    puts ""
+  else
+    matches.each do |match|
+      puts match
+    end
+  end
+end
+
+reg_pattern(ARGV)
