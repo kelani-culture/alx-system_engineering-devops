34aa6fdde1db40d57f2b1843ba2d5c51d17873bd
Null <kelanidarasimi9@gmail.com>
2023-11-24 23:33:36 +0100

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-# create a file in the /tmp folder
+
+# a  puppet file that create a content and write to its
 file { '/tmp/school':
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
+  ensure  => present,
+  mode    => '0744'.
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# kill a process called killmenow
-
-exec { 'kill_killmenow_process':
-command => 'pkill -f killmenow',
-path    => ['/usr/bin', '/bin'],
-onlyif  => 'pgrep -f killmenow',
-}
