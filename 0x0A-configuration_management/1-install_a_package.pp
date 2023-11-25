4aba1f4ab539b71015a21e2e269618c5ea580372
Null <kelanidarasimi9@gmail.com>
2023-11-26 00:32:43 +0100

    task 1 redids

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 #install flask
-
 file { '/usr/bin/pip3':
   ensure => present,
 }
@@ -7,4 +6,9 @@ file { '/usr/bin/pip3':
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
