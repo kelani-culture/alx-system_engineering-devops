49b096580057aba1c52e3435446ef4de763cc62b
Null <kelanidarasimi9@gmail.com>
2023-11-25 23:23:50 +0100

    task 1 redid

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 # a  puppet file that create a content and write to its
 file { '/tmp/school':
   ensure  => present,
-  mode    => '0744'.
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
