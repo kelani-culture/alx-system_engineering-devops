59dae4f0363bd6650f77670eb8c607f2bfad0b9c
Null <kelanidarasimi9@gmail.com>
2023-11-26 00:28:54 +0100

    task 2 redids

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #lill a precess called killmenow
 exec { '/usr/bin/pkill':
-command      => '/usr/bin/pkill -f killmenow',
-onlyif       => '/usr/bin/pgrep -f killmenow'
+command => '/usr/bin/pkill -f killmenow',
+onlyif  => '/usr/bin/pgrep -f killmenow',
 }
