b73c942442e6c0b59655e2bb3fff116962bfb19d
Null <kelanidarasimi9@gmail.com>
2023-11-29 19:14:57 +0100

    task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,32 @@
-#install nginx and configure server
+# Update package lists
 exec { 'update':
   command => 'sudo apt-get -y update',
   path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-package {'nginx':
-  ensure => installed,
+# Install NGINX
+package { 'nginx':
+  ensure   => installed,
   provider => 'apt',
 }
 
-service {'nginx':
+# Ensure NGINX service is running and enabled
+service { 'nginx':
   ensure     => running,
   enable     => true,
   hasrestart => true,
 }
 
-file {'/etc/var/www/html/index.nginx-debian.html':
+# Create the default HTML file
+file { '/var/www/html/index.nginx-debian.html':
   ensure  => present,
   content => 'Hello World!',
 }
 
+# Configure NGINX default site with a 301 redirect
 file { '/etc/nginx/sites-available/default':
   ensure       => present,
   content      => "server {\n\tlisten 80 default_server;\n\tserver_name _;\n\troot /var/www/html;\n\tlocation = /redirect_me{\n\t\treturn 301 https://regex101.com/;\n\t}\n}",
   validate_cmd => 'sudo /usr/sbin/nginx -t',
-  require      => [Service['nginx']],
+  require      => Service['nginx'],
 }
