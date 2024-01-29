db30f818d69c8214bd2720da603f8ca4151a9bd1
Null <kelanidarasimi9@gmail.com>
2024-01-29 12:32:30 +0100

    write to a json 4

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,7 +17,7 @@ def employee_data(emp_id):
     total_task = len([task.get('completed') for task in task_info])
     task_completed = sum(task.get('completed') for task in task_info)
 
-    print(f"{name} is done with tasks({task_completed}/{total_task}):")
+    print(f"Employee {name} is done with tasks({task_completed}/{total_task}):")
     for task in task_info:
         print(f"\t{task['title']}")
 
