44bef865e1062d40871b42c2c9c3a7ecc26fddef
Null <kelanidarasimi9@gmail.com>
2024-04-09 12:30:12 +0100

    task 2 redid

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ titles of the first 10 hot posts listed for a given subreddit.
 import requests
 import json
 
-def top_ten(subreddit: str) -> str:
+def top_ten(subreddit: str) -> any:
     """
     return top 10 host posts
     """
@@ -16,12 +16,11 @@ def top_ten(subreddit: str) -> str:
     response = requests.get(f'https://www.reddit.com/r/{subreddit}/hot.json?limit=9', headers=headers)
     v = json.dumps(response.json(), indent = 3)    
     data = response.json()
-    print(v)
     if 'data' in data and 'children' in data['data']:
         res = []
         val = data['data']['children']
         for val in val:
             res.append(val['data']['title'])
         return '\n'.join(res)
-    print(None)
+    return None
 print(top_ten('programming'))
